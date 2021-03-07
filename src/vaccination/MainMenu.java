dac2bb2cd3826ddbd2cefd3386005f6259533a68
Roland <enzinger@parkside.at>
2021-03-07 20:51:10 +0100

    initial commit

---
+++ src/vaccination/MainMenu.java
@@ -0,0 +1,16 @@
+package vaccination;
+
+public class MainMenu {
+
+	public MainMenu() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello 2BIT");
+
+	}
+
+}
---
+++ test/vaccination/testMainMenu.java
@@ -0,0 +1,14 @@
+package vaccination;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testMainMenu {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
