362a6446cee1a7c0d6a79702ccc545cd35a8458d
Kryuk <doomfich.groeger@gmail.com>
2021-03-11 11:34:38 +0100

    Konstrukto

--- src/vaccination/timeslot.java
+++ src/vaccination/timeslot.java
@@ -1,21 +1,23 @@
 package vaccination;
 
 public class timeslot {
-	private int slotId;
-	private int slotStart;
-	private int slotEnd;
-	private int slotDate;
-	private int slotScheduled;
+	private int[] slotId;
+	private int[] slotStart;
+	private int[] slotEnd;
+	private String[] slotDate;
+	private int[] slotScheduled;
 	
-	public timeslot (int slotId,int slotStart,int slotEnd,int slotDate, int slotScheduled) {
+	public timeslot (int[] slotId,int[] slotStart,int[] slotEnd,String[] slotDate, int[] slotScheduled) {
 		this.slotId = slotId;
 		this.slotStart = slotStart;
+		this.slotEnd = slotEnd;
+		this.slotDate = slotDate;
+		this.slotScheduled = slotScheduled;
 		
 	}
 	
 	
 	public static int addSlot() {
-		
 	return 0;
 	}
 	public static int deleteUnscheduledSlot() {
