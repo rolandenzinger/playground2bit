e2c319852dee27c978ca13ba0fd4beac15831822
Kryuk <doomfich.groeger@gmail.com>
2021-03-11 11:34:30 +0100

    Konstruktor

--- src/vaccination/timeslot.java
+++ src/vaccination/timeslot.java
@@ -1,15 +1,26 @@
 package vaccination;
 
 public class timeslot {
+	private int slotId;
+	private int slotStart;
+	private int slotEnd;
+	private int slotDate;
+	private int slotScheduled;
+	
+	public timeslot (int slotId,int slotStart,int slotEnd,int slotDate, int slotScheduled) {
+		this.slotId = slotId;
+		this.slotStart = slotStart;
+		
+	}
 	
 	
 	public static int addSlot() {
 		
-		return 0;
+	return 0;
 	}
 	public static int deleteUnscheduledSlot() {
 		
-		return 0;
+	return 0;
 	}
 	public static int addScheduledSlotforPerson() {
 	
