58cd6f8ba6d254ca8ec96565d6aea7b70be8dcd3
FloAntl <80036219+FloAntl@users.noreply.github.com>
2021-03-11 11:39:13 +0100

    Die Methode addNewPerson wurde angefangen, wird von anderen fortgesetzt.

--- src/vaccination/Person.java
+++ src/vaccination/Person.java
@@ -6,6 +6,15 @@ public class Person {
 	
 	//gibt die personID zurück
 	public static int addNewPerson(String firstName, String lastName, int socialSecurityNumber, String street, int ZIP, String City, String Country, String Birthday, int Telephone) {
+		String[][] temp = person;
+		person = null;
+		
+		person = new String[temp.length + 1][10];
+		for(int i = 0; i < temp.length; i++) {
+			person[i] = temp[i];
+		}
+		
+		person[temp.length][0] = firstName;
 		return 0;
 	}
 	
