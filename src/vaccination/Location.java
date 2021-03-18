4eca6f0689b02ac5424ef1afaeea5cfc4faac8bc
Jamokl <jannik.goelles@a1.net>
2021-03-18 10:21:35 +0100

    Änderung an addLocation

--- src/vaccination/Location.java
+++ src/vaccination/Location.java
@@ -4,19 +4,25 @@ public class Location {
 	public int LocationID;
 	public String Name;
 	public String Street;
-	public String ZIP;
+	public int ZIP;
 	public String City;
 	public String Country;
+	public String[][] locations = new String[1][6];
 
-	public int addLocation(String TName, String TStreet, String TZIP, String TCity, String TCOuntry) {
-
-		return 1;
+	public String[][] addLocation(int TLocationID, String TName, String TStreet, int TZIP, String TCity, String TCOuntry) {
+		locations[0][0] = Integer.toString(TLocationID);
+		locations[0][1] = TName;
+		locations[0][2] = TStreet;
+		locations[0][3] = Integer.toString(TZIP);
+		locations[0][4] = TCity;
+		locations[0][5] = TCOuntry;
+		return locations;
 	}
 
-	public Boolean deleteLocation(String TName, String TStreet, String TZIP, String TCity, String TCOuntry) {
+	public Boolean deleteLocation(int TLocationID, String TName, String TStreet, int TZIP, String TCity, String TCOuntry) {
 		return true;
 	}
 	public void listAllLocations() {
-		System.out.println("Baumax Leoben");
+		System.out.println(locations);
 	}
 }
