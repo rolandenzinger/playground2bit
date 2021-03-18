77fb231cbdfa4d447198840b02e64e10155f9d96
Shankara Ulbrich <ulbrichshankara@gmail.com>
2021-03-18 11:19:37 +0100

    Grungerüst fertig ----------------- Aufgaben:
    Alles mit ** Gehört eingebunden (Klassen von anderen)
    Alles System.out.print/println gehören mit der methode "saysomething" von Kopp ausgegeben.
    Rechtschreibprüfung
    Programm in Schleife setzen, beenden Funktion schreiben und in die Fuktion "Mainmenue" Einbinden
    Text für "ÜberApplikation" Entwickeln.
    Testen ob alles Funktioniert
    Spaß haben XD

--- src/vaccination/MainMenu.java
+++ src/vaccination/MainMenu.java
@@ -1,6 +1,15 @@
 package vaccination;
 
 import java.util.Scanner;
+//todo
+//Alles mit ** Gehört eingebunden (Klassen von anderen)
+//Alles System.out.print/println gehören mit der methode "saysomething" von Kopp ausgegeben.
+//Rechtschreibprüfung
+//Programm in Schleife setzen, beenden Funktion schreiben und in die Fuktion "Mainmenue" Einbinden
+//Text für "ÜberApplikation" Entwickeln.
+//Testen ob alles Funktioniert
+//Spaß haben XD
+
 
 public class MainMenu {
 
@@ -10,36 +19,43 @@ public class MainMenu {
         try {
             int userInput = 0;
             System.out.println("Geben Sie (1) für Standortverwalung ein.");
-            System.out.println("Geben Sie (2) ein um das Impf-Zeitfenster zu sehen.");
+            System.out.println("Geben Sie (2) ein um auf das Impf-Zeitfenster zuzugreifen.");
             System.out.println("Geben Sie (3) für Terminvergabe ein.");
             System.out.println("Geben Sie (4) für Personenverwaltung ein.");
             System.out.println("Geben Sie (5) um mehr Über diese Applikation zu erfahren.");
-            System.out.println("Geben Sie (6) ein um das Prrogramm zu beenden");
+            System.out.println("Geben Sie (6) ein um das Programm zu beenden");
             userInput = Integer.parseInt(sc.nextLine());
             MainMenu(userInput);
         } catch (NumberFormatException e) {
             System.out.println("Bitte geben Sie eine Zahl ein.");
         }
     }
+
     public static void MainMenu(int input) {
 
 
         switch (input) {
             case 1:
-                output("1: Standortverwaltung");
+                Standortverwaltung();
+                break;
             case 2:
-                output("2: Imf-Zeitfenster");
+                ImpfZeitfenster();
+                break;
             case 3:
-                output("3: Terminvergabe");
+                Terminvergabe();
+                break;
             case 4:
-                output("4: Personenverwaltung");
+                Personenverwaltung();
+                break;
             case 5:
-                output("5: Über diese Applikation");
+                Informationen();
+                break;
             case 6:
-                output("6: Beenden");
-			default:
-				output("Ungülltige eingabe");
-
+                output("'Beenden'");
+                break;
+            default:
+                output("Fehler: falsche Zahl");
+                break;
         }
 
 
@@ -49,4 +65,124 @@ public class MainMenu {
         System.out.println(out);
     }
 
+    public static void Standortverwaltung() {
+        int userInput = 0;
+        System.out.println("Sie befinden sich im Untermenü 'Standortverwaltung'");
+        System.out.println("Geben Sie (1) ein um einen neuen Standort hinzuzufügen");
+        System.out.println("Geben Sie (2) ein um einen Standot zu löschen");
+        System.out.println("Geben Sie (3)  ein um alle bereits vorhanden Standorte zu listen ");
+        try {
+            userInput = Integer.parseInt(sc.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Bitte geben Sie eine Zahl ein.");
+        }
+        switch (userInput) {
+            case 1:
+                System.out.println("*Standort hinzugefügt*");
+                break;
+            case 2:
+                System.out.println("*Standort gelöscht*");
+                break;
+            case 3:
+                System.out.println("*Standorte!!!!! *");
+                break;
+            default:
+                System.out.println("Falsche Nummer");
+                break;
+        }
+
+    }
+
+    public static void ImpfZeitfenster() {
+        int userInput = 0;
+        System.out.println("Sie befinden sich im Untermenü 'Impf-Zeitfenster'");
+        System.out.println("Geben Sie (1) ein um ein Freies Impfenster anzulegen.");
+        System.out.println("Geben Sie (2) um ein ungebuchtes Impf-Zeitfenster zu löschen.");
+        try {
+            userInput = Integer.parseInt(sc.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Bitte geben Sie eine Zahl ein.");
+        }
+        switch (userInput) {
+            case 1:
+                System.out.println("*Freies Impf-Zeitfenster angelegt*");
+                break;
+            case 2:
+                System.out.println("*Ungebuchtes Impf-Zeitfenster gelöscht*");
+                break;
+            default:
+                System.out.println("Falsche Nummer");
+                break;
+        }
+
+    }
+
+    public static void Terminvergabe() {
+        int userInput = 0;
+        System.out.println("Sie befinden sich im Untermenü 'Terminvergabe'");
+        System.out.println("Geben Sie (1) um freie Termine für Ihren Standort anzuzeigen.");
+        System.out.println("Geben Sie (2) um freie Termine für ein Datum anzuzeigen.");
+        System.out.println("Geben Sie (3) um freie Termine zu buchen.");
+        System.out.println("Geben Sie (4) um einen Gebuchten Termin zu löschen.");
+        try {
+            userInput = Integer.parseInt(sc.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Bitte geben Sie eine Zahl ein.");
+        }
+        switch (userInput) {
+            case 1:
+                System.out.println("*Termine für Standort*");
+                break;
+            case 2:
+                System.out.println("*Freie Termine für Datum*");
+                break;
+            case 3:
+                System.out.println("*Freier Termin gebucht!*");
+                break;
+            case 4:
+                System.out.println("*Gebuchter Termin gelöscht!");
+                break;
+            default:
+                System.out.println("Falsche Nummer");
+                break;
+        }
+
+    }
+
+    public static void Personenverwaltung() {
+        int userInput = 0;
+        System.out.println("Sie befinden sich im Untermenü 'Personenverwaltung'");
+        System.out.println("Geben Sie (1) um eine neue Person anzulegen.");
+        System.out.println("Geben Sie (2) um eine Person zu löschen.");
+        System.out.println("Geben Sie (3 ein um Alle Personen zu listen)");
+
+        try {
+            userInput = Integer.parseInt(sc.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Bitte geben Sie eine Zahl ein.");
+        }
+
+        switch (userInput) {
+            case 1:
+                System.out.println("*Person angelegt!!*");
+                break;
+            case 2:
+                System.out.println("*Person gelöscht*");
+                break;
+            case 3:
+                System.out.println("*Alle Personen !!!!!*");
+            default:
+                System.out.println("Falsche Nummer");
+                break;
+        }
+
+    }
+
+    public static void Informationen() {
+        System.out.println("Informationen!!!!!!!!!");
+    }
+
+
 }
+
+
--- test/vaccination/testMainMenu.java
+++ test/vaccination/testMainMenu.java
@@ -1,14 +1,8 @@
 package vaccination;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
 
 class testMainMenu {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+
 
 }
