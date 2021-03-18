abfb78a788540ab883293fc76b33b1db62845153
FloAntl <flo.kein.1000000@outlook.com>
2021-03-18 09:40:58 +0100

    Test für addNewPerson wurden angefangen und werden später fortgesetzt.

---
+++ test/vaccination/testPerson.java
@@ -0,0 +1,16 @@
+package vaccination;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testPerson {
+
+	@Test public void testAddNewPerson() {
+		assertEquals(0, Person.addNewPerson("Maxi", "Mustermann", 123456789, "Kastanienweg", 8740, "Weiﬂkirchen", "Austria", "12.12.20012", 6801506));
+		assertEquals(1, Person.addNewPerson("Maxi1", "Mustermann", 123456789, "Kastanienweg", 8740, "Weiﬂkirchen", "Austria", "12.12.20012", 6801506));
+		assertEquals(2, Person.addNewPerson("Maxi2", "Mustermann", 123456789, "Kastanienweg", 8740, "Weiﬂkirchen", "Austria", "12.12.20012", 6801506));
+		assertEquals(3, Person.addNewPerson("Maxi3", "Mustermann", 123456789, "Kastanienweg", 8740, "Weiﬂkirchen", "Austria", "12.12.20012", 6801506));
+	}
+
+}
