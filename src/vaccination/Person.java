dcbbeda0b33c2ddafd8abb9227feff8c95afc143
FloAntl <flo.kein.1000000@outlook.com>
2021-03-18 09:24:02 +0100

    Fehlerbehebung in addNewPerson

--- src/vaccination/Person.java
+++ src/vaccination/Person.java
@@ -9,7 +9,7 @@ private static String[][] person = null;
 	public static int addNewPerson(String firstName, String lastName, int socialSecurityNumber, String street, int ZIP, String City, String Country, String Birthday, int Telephone) {
 
 		if (person == null) {
-			person = new String[1][10];
+			person = new String[1][9];
 			person[0][0] = firstName + "";
 			person[0][1] =lastName + "";
 			person[0][2] = socialSecurityNumber + "";
@@ -19,27 +19,29 @@ private static String[][] person = null;
 			person[0][6] = Country + "";
 			person[0][7] = Birthday + "";
 			person[0][8] = Telephone + "";
+			return 0;
+		}else {
+			
+			String[][] temp = person;
+			person = null;
+			
+			person = new String[temp.length + 1][10];
+			for(int i = 0; i < temp.length; i++) {
+				person[i] = temp[i];
+			}
+			
+			person[temp.length][0] = firstName + "";
+			person[temp.length][1] =lastName + "";
+			person[temp.length][2] = socialSecurityNumber + "";
+			person[temp.length][3] = street + "";
+			person[temp.length][4] = ZIP + "";
+			person[temp.length][5] = City + "";
+			person[temp.length][6] = Country + "";
+			person[temp.length][7] = Birthday + "";
+			person[temp.length][8] = Telephone + "";
+			
+			return person.length -1;
 		}
-		
-		String[][] temp = person;
-		person = null;
-		
-		person = new String[temp.length + 1][10];
-		for(int i = 0; i < temp.length; i++) {
-			person[i] = temp[i];
-		}
-		
-		person[temp.length][0] = firstName + "";
-		person[temp.length][1] =lastName + "";
-		person[temp.length][2] = socialSecurityNumber + "";
-		person[temp.length][3] = street + "";
-		person[temp.length][4] = ZIP + "";
-		person[temp.length][5] = City + "";
-		person[temp.length][6] = Country + "";
-		person[temp.length][7] = Birthday + "";
-		person[temp.length][8] = Telephone + "";
-		
-		return person.length -1;
 	}
 	
 	//gibt false zurück, wenn das Löschen nicht erfolgreich war
