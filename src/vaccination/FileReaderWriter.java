caeb89d2f3cb54144bd049147afbeb598879f15b
Dominik <Dominik@172.21.113.126>
2021-03-18 10:58:00 +0100

    Test1

--- src/vaccination/FileReaderWriter.java
+++ src/vaccination/FileReaderWriter.java
@@ -1,5 +1,31 @@
 package vaccination;
 
 public class FileReaderWriter {
+	
+	public String writeEntry(int Key, int ID, String info1, String info2, String info3, String info4, String info5, String info6, String info7, String info8, String info9, String info10) {
+		
+		return null; 
+	}
+	
+	public String ReadEntry(int Key, int ID) {
+		
+		return null; 
+	}
+	
+	public String DeleteEntry(int Key, int ID) {
+		
+		return null; 
+	}
+	
+	public String ReadAllEntriesWithKey(int Key) {
+		
+		return null; 
+	}
+	
+	public String GetInfoFromBlock() {
+		
+		return null;
+	}
 
 }
+
