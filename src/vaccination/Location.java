b759db46329d2a691b67305a8c80f4446e6e9e60
draneas1 <andreas.ficzko@gmail.com>
2021-03-25 09:36:15 +0100

    listAllLocations_fertig

--- src/vaccination/Location.java
+++ src/vaccination/Location.java
@@ -29,6 +29,11 @@ public class Location {
 		return locations;
 	}
 	public void listAllLocations() {
-		System.out.println(locations);
+		for(int i = 0; i < locations.length; i++) {
+			for(int o = 0; o < locations[0].length; o++) {
+				System.out.print(locations[i][o] + ", ");
+			}
+			System.out.println("");
+		}
 	}
 }
